a15b0820f3450c828d7acd0c6f33b7908babccc4
SlicR97 <32324514+SlicR97@users.noreply.github.com>
2024-12-03 17:58:21 +0000

    Prep Work

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,44 @@
 module Main (main) where
 
-import Lib
+import qualified Data.Foldable as DF(mapM_)
+import qualified Data.Functor as F((<&>))
+import qualified Data.Map as Map(Map, fromList, lookup, toList)
+import qualified System.Environment as S(getArgs)
+
+import qualified Challenge00 as C00(solve)
+
+solvers :: Map.Map String (String -> String)
+solvers = Map.fromList
+    [ ("00", C00.solve)
+    ]
+
+parseArgs :: [String] -> Maybe String
+parseArgs (a:_) = Just a
+parseArgs [] = Nothing
+
+readInput :: String -> IO String
+readInput challengeNumber =
+    let fileName = "app/inputs/input-" ++ challengeNumber in
+    readFile fileName
+
+solveChallenge :: String -> IO ()
+solveChallenge cn = do
+    input <- readInput cn
+    let ms = Map.lookup cn solvers
+    case ms of
+        Nothing -> return ()
+        Just s -> putStrLn $ s input
+
+solveAllChallenges :: () -> IO ()
+solveAllChallenges () = do
+    DF.mapM_ solveSingleChallenge $ Map.toList solvers
+    where solveSingleChallenge (cn, solver) = do
+            input <- readInput cn
+            putStrLn $ solver input
 
 main :: IO ()
-main = someFunc
+main = do
+    challengeName <- S.getArgs F.<&> parseArgs
+    case challengeName of
+        Just cn -> solveChallenge cn
+        Nothing -> solveAllChallenges ()
---
+++ src/Challenge00.hs
@@ -0,0 +1,4 @@
+module Challenge00(solve) where
+
+solve :: String -> String
+solve input = input ++ " output"
